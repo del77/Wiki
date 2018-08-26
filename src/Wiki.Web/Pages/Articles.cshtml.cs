ed028c120566d1c360d884916ec102bb311f70f8
Jakub Przybylski <xdelpl@gmail.com>
2018-08-26 22:05:58 +0200

    bigfoses

--- src/Wiki.Web/Pages/Articles.cshtml.cs
+++ src/Wiki.Web/Pages/Articles.cshtml.cs
@@ -30,6 +30,8 @@ namespace Wiki.Web.Pages
 
         [BindProperty]
         public Filter Filter { get; set; }
+        [BindProperty]
+        public BrowseFilter BrowseFilter { get; set; }
         public bool CanRead { get; set; }
 
         public ArticlesModel(IArticleService articleService, IHttpContextAccessor httpContextAccessor)
@@ -44,18 +46,86 @@ namespace Wiki.Web.Pages
 
         public async Task<IActionResult> OnGetAsync(string title, IEnumerable<int> selectedTags, int selectedCategory, int selectedStatus)
         {
-            SetupFilter(title, selectedTags, selectedCategory);
-            if(!CanRead && selectedStatus != 0)
+            //SetupFilter(title, selectedTags, selectedCategory);
+            Filter = new Filter
+            {
+                Categories = new SelectList(new int[] {}),
+                SelectedTags = new List<int>(),
+                Statuses = new SelectList(new int[] {}),
+                Tags = new List<TagFilter>()
+
+            };
+            json = JsonConvert.SerializeObject(Test);
+            BrowseFilter = new BrowseFilter();
+            BrowseFilter.Categories = JsonConvert.SerializeObject(new List<string>() { "cat1", "cat2", "cat3" });
+            BrowseFilter.Titles = JsonConvert.SerializeObject(new List<string>() { "article1", "article2", "article3" });
+
+            if (!CanRead && selectedStatus != 0)
             {
                 return Page();
             }
             if (!CanRead)
                 selectedStatus = 1;
-            var res = await articleService.BrowseAsync(title, selectedTags, selectedCategory, selectedStatus);
-            var res2 = (await articleService.BrowseAsync(title, selectedTags, selectedCategory, selectedStatus)).ToPagedList(1, 5);
+            //var res = await articleService.BrowseAsync(title, selectedTags, selectedCategory, selectedStatus);
+            //var res2 = (await articleService.BrowseAsync(title, selectedTags, selectedCategory, selectedStatus)).ToPagedList(1, 5);
 
             Articles = new List<ViewModels.Article>();
             
+            List<ArticleDto> test = new List<ArticleDto>();
+            var a1 = new ArticleDto();
+            a1.Category = new ArticleCategoryDto
+            {
+                Category = "cat1",
+                Id = 1
+            };
+            var t1 = new TextDetailsDto
+            {
+                Status = new TextStatusDto
+                {
+                    Id = 1,
+                    Status = "stat1"
+                },
+                Title = "article1"
+            };
+            var tags1 = new List<TextTagDto>();
+            tags1.Add(new TextTagDto
+            {
+                Id = 1,
+                Tag = "tag1"
+            });
+            t1.Tags = tags1;
+            a1.Master = t1;
+            a1.Texts = new List<TextDto>() {t1};
+
+            var a2 = new ArticleDto();
+            a2.Category = new ArticleCategoryDto
+            {
+                Category = "cat2",
+                Id = 2
+            };
+            var t2 = new TextDetailsDto
+            {
+                Status = new TextStatusDto
+                {
+                    Id = 2,
+                    Status = "stat2"
+                },
+                Title = "article2"
+            };
+            var tags2 = new List<TextTagDto>();
+            tags2.Add(new TextTagDto
+            {
+                Id = 2,
+                Tag = "tag2"
+            });
+            t2.Tags = tags2;
+            a2.Master = t2;
+            a2.Texts = new List<TextDto>() { t2 };
+
+            test.Add(a1);
+            test.Add(a2);
+            var res = test;
+
             foreach (var item in res)
             {
                 foreach (var text in item.Texts)
---
+++ src/Wiki.Web/ViewModels/BrowseFilter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Wiki.Web.ViewModels
+{
+    public class BrowseFilter
+    {
+        public string Titles { get; set; }
+        public string Categories { get; set; }
+        public string Statuses { get; set; }
+    }
+}
